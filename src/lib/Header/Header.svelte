9e478ccea3e5540223b0f57f10e5366547d8b99d
jbailey111 <jake.bailey2599@gmail.com>
2022-08-02 00:04:30 +0100

    Combined header title and menu breadcrumbs into singular div

--- src/lib/Header/Header.svelte
+++ src/lib/Header/Header.svelte
@@ -8,17 +8,17 @@
     <div class="nav-item" />
     <div class="nav-item">
       <span class="title">Svelte Shop</span>
+      <div class="menu">
+        <div class="menu-item">Home</div>
+        <div class="menu-item">About</div>
+        <div class="menu-item">Contact</div>
+      </div>
     </div>
     <div class="nav-item icons">
       <div />
       <div class="menu-item">Login</div>
       <Basket />
     </div>
-  </div>
-  <div class="menu">
-    <div class="menu-item">Home</div>
-    <div class="menu-item">About</div>
-    <div class="menu-item">Contact</div>
   </div>
 </nav>
 
