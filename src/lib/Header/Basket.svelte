d954aaea26230a8ee3b51a7e9dc7e42cec6239a5
jbailey111 <jake.bailey2599@gmail.com>
2022-08-01 23:53:29 +0100

    Created header (#1)

---
+++ src/lib/Header/Basket.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "./Header.scss";
+</script>
+
+<div>
+  <div class="basket">
+    <div>
+      <span class="basket-text">Basket</span>
+    </div>
+    <div>
+      <span class="basket-text">0</span>
+    </div>
+  </div>
+</div>
---
+++ src/lib/Header/Header.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import "./Header.scss";
+  import Basket from "./Basket.svelte";
+</script>
+
+<nav>
+  <div class="navbar">
+    <div class="nav-item" />
+    <div class="nav-item">
+      <span class="title">Svelte Shop</span>
+    </div>
+    <div class="nav-item icons">
+      <div />
+      <div class="menu-item">Login</div>
+      <Basket />
+    </div>
+  </div>
+  <div class="menu">
+    <div class="menu-item">Home</div>
+    <div class="menu-item">About</div>
+    <div class="menu-item">Contact</div>
+  </div>
+</nav>
+
+<style>
+</style>
