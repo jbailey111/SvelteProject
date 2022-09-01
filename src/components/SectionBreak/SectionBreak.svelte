1c750353d448c5a5e0b600262c6d54ea9b98003e
jbailey111 <jake.bailey2599@gmail.com>
2022-09-01 20:22:22 +0100

    Section break component

---
+++ src/components/SectionBreak/SectionBreak.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+</script>
+
+<div class="section-break" />
+
+<style>
+    .section-break {
+        width: 100%;
+        height: 2px;
+        background-color: #e6e6e6;
+        margin: 0.5rem;
+        border-radius: 10px;
+    }
+</style>
