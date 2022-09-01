dbb2f02b5f7d2f7b14435e413bf4a61b53394438
jbailey111 <jake.bailey2599@gmail.com>
2022-09-01 20:22:07 +0100

    Option selector component

---
+++ src/components/OptionSelector/OptionSelector.stories.js
@@ -0,0 +1,11 @@
+import OptionSelector from './OptionSelector.svelte';
+//import app.scss
+export default {
+    title: 'Options',
+    component: OptionSelector,
+};
+
+export const Default = () => ({
+    Component: OptionSelector,
+    props: {},
+});
---
+++ src/components/OptionSelector/OptionSelector.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import './OptionSelector.scss';
+</script>
+
+<div class="option-wrapper">
+    <div class="options-title-container">
+        <span class="option-title">Colours</span>
+    </div>
+    <div class="options">
+        <div style="--color: {'red'}" class="colour-option" />
+        <div
+            style="--color: {'green'}"
+            class="colour-option"
+            data-value="green"
+        />
+        <div
+            style="--color: {'blue'}"
+            class="colour-option"
+            data-value="blue"
+        />
+    </div>
+</div>
+
+<style lang="scss">
+    .colour-option {
+        width: 50px;
+        background-color: var(--color);
+        height: 50px;
+        border-radius: 10%;
+    }
+</style>
